518897f872e5d117810efc510ece47b1ef17576c
chief-priest <sylviapascale@yahoo.com>
2026-08-22 19:30:13 -0500

    Test compliance gate with insecure S3 configuration

--- terraform/s3-security.tf
+++ terraform/s3-security.tf
@@ -1,11 +1,11 @@
-resource "aws_s3_bucket_public_access_block" "compliance_demo" {
-  bucket = aws_s3_bucket.compliance_demo.id
+# resource "aws_s3_bucket_public_access_block" "compliance_demo" {
+#   bucket = aws_s3_bucket.compliance_demo.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
 
 resource "aws_s3_bucket_versioning" "compliance_demo" {
   bucket = aws_s3_bucket.compliance_demo.id
